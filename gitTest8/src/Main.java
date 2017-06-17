5ea90506cb7a51c51cbe92aef5ceeea810891e20
dganenia <dg.anenia@gmail.com>
2017-06-17 13:43:45 -0400

    created test8

---
+++ gitTest8/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyData data = new MyData();
+		
+		greet();
+		data.display();
+	}
+	
+	private static void greet() {
+		
+		System.out.println("Hello... v.1");
+		
+	}
+}
---
+++ gitTest8/src/MyData.java
@@ -0,0 +1,12 @@
+
+public class MyData {
+
+	int data[] = {1,3,5};
+	
+	public void display() {
+		for (int d : data) {
+			System.out.println("Data: " + d);
+		}
+		
+	}
+}
